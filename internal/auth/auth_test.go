65eb61bace2768c4350b9b92a4712e35c46b3adc
dipzza <dipzza@disroot.org>
2025-04-13 14:46:50 +0200

    Format tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -8,43 +8,42 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-
 func TestGetAPIKey(t *testing.T) {
 	tests := map[string]struct {
-			input 	http.Header
-			wantKey string
-			wantErr error
+		input   http.Header
+		wantKey string
+		wantErr error
 	}{
-			"simple": {
-				input: http.Header{"Authorization": []string{"ApiKey aKey"}},
-				wantKey: "aKey",
-				wantErr: nil,
-			},
-			"empty": {
-				input: http.Header{"Authorization": []string{}},
-				wantKey: "",
-				wantErr: ErrNoAuthHeaderIncluded,
-			},
-			"malformed": {
-				input: http.Header{"Authorization": []string{"ApiKey"}},
-				wantKey: "",
-				wantErr: ErrMalformedAuthHeader,
-			},
+		"simple": {
+			input:   http.Header{"Authorization": []string{"ApiKey aKey"}},
+			wantKey: "aKey",
+			wantErr: nil,
+		},
+		"empty": {
+			input:   http.Header{"Authorization": []string{}},
+			wantKey: "",
+			wantErr: ErrNoAuthHeaderIncluded,
+		},
+		"malformed": {
+			input:   http.Header{"Authorization": []string{"ApiKey"}},
+			wantKey: "",
+			wantErr: ErrMalformedAuthHeader,
+		},
 	}
 
 	for name, tc := range tests {
-			t.Run(name, func(t *testing.T) {
-				key, err := GetAPIKey(tc.input)
+		t.Run(name, func(t *testing.T) {
+			key, err := GetAPIKey(tc.input)
 
-				keyDiff := cmp.Diff(tc.wantKey, key)
-				if keyDiff != "" {
-					t.Fatal(keyDiff)
-				}
+			keyDiff := cmp.Diff(tc.wantKey, key)
+			if keyDiff != "" {
+				t.Fatal(keyDiff)
+			}
 
-				errDiff := cmp.Diff(tc.wantErr, err, cmpopts.EquateErrors())
-				if errDiff != "" {
-					t.Fatal(errDiff)
-				}
-			})
+			errDiff := cmp.Diff(tc.wantErr, err, cmpopts.EquateErrors())
+			if errDiff != "" {
+				t.Fatal(errDiff)
+			}
+		})
 	}
 }
